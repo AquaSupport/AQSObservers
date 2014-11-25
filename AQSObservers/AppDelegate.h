fb8d4c31872398ee7bf69db27400971c03ca9e40
Kai INUI <lied.der.optik@gmail.com>
2014-11-25 12:46:43 +0900

    init

---
+++ AQSObservers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AQSObservers
+//
+//  Created by kaiinui on 2014/11/25.
+//  Copyright (c) 2014年 Aquamarine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
